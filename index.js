54180ca8a22ab45bd28bbdd1d079c8a3cf1d74db
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2019-12-30 22:29:27 +0530

    API server created
    a server for API is created and we can read data from server.

---
+++ index.js
@@ -0,0 +1,83 @@
+const fs= require('fs');
+const http= require('http');
+const url= require('url');
+////////////////////////////////////////////////////////////////////////////////
+
+//Blocking synchronous  way 
+//const textin = fs.readFileSync( './txt/input.txt','utf8');
+//console.log(textin);
+
+ //const textout= `This is what we know about the avocado:${textin}`;
+ //fs.writeFileSync('./txt/output.txt',textout);
+// console.log("file Written!");
+  
+ // Non-Blocking Asynchronous way.
+//
+/*fs.readFile('.txt/start.txt','utf-8', (err,data1)=>{
+     if(err) return console.log('error');
+    fs.writeFile(`.txt/${data1}.txt`,'utf-8', (err,data2)=>{
+    console.log(data2);
+    });
+});
+console.log("will read file!");
+*///
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+//SERVER
+ 
+const server=http.createServer((req,res)=>{
+    console.log(req.url);  
+    const pathname = req.url;
+    if(pathname === '/over'|| pathname==='/')
+    {
+        res.end('this is the overview');
+
+    } 
+
+    else if(pathname==='/product'){
+        res.end('product ');
+    }
+    else if(pathname ==='/api'){
+        fs.readFile (`${__dirname}/dev-data/data.json`,'utf-8',(err,data)=>
+        {
+            productData=JSON.parse(data);
+            console.log(productData);
+        }
+        );
+
+        
+        res.end('API');
+    } else{
+        res.writeHead(404);
+        res.end('<h1> page not found!</h1>');
+    }
+   
+});
+
+server.listen(8000,'127.0.0.1',()=>{
+    console.log('server has been started');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
